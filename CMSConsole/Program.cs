2695c38ddc87d20d19adf5df59fcaf8663e7e51b
pauluxxx <pauluxxx@mail.ru>
2017-03-05 16:25:00 +0200

    ad files

---
+++ CMSConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using CMSTeams.Controllers;
+using CMSTeams.Models;
+using CMSTeams.Models.Core;
+using CMSTeams.Models.Singletone;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMSConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Application.Initialize(ConfigurationHelper.ConfigureDependencies);
+            var injectedEngine = new Engine();
+            var basketPage = PageStore.Pages.OfType<BasketPage>().FirstOrDefault();
+            var basketController = new BasketController(basketPage,injectedEngine);
+            basketController.Index();
+            var registerPage = PageStore.Pages.OfType<RegisterPage>().FirstOrDefault();
+            var registerController = new RegisterController(registerPage, injectedEngine);
+            registerController.Index();
+
+            Console.ReadKey();
+           
+        }
+    }
+}
